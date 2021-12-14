78dbfce163cd63b51e48bf6eb69d133528920ea2
Matthew Kiss <matthewkiss@Matthews-MacBook-Air.local>
2021-12-14 18:38:24 +0000

    add comment card

--- components/Post.js
+++ components/Post.js
@@ -11,7 +11,7 @@ import { HeartIcon as HeartIconFilled } from '@heroicons/react/solid'
 
 function Post({ id, username, profilePic, img, caption }) {
     return (
-        <div className='bg-white m-7 border rounded-sm'>
+        <div className='bg-white my-7 border rounded-sm '>
             {/*Header*/}
             <div className="flex items-center p-5">
                 <img src={profilePic} alt='profile' className='rounded-full h-12 w-12 border p-1 mr-3' />
@@ -19,8 +19,21 @@ function Post({ id, username, profilePic, img, caption }) {
                 <DotsHorizontalIcon className='h-5' />
             </div>
             {/*Image*/}
+            <img src={img} alt='pic' className='object-cover w-full' />
+
             {/*Buttons*/}
+            <div className='flex justify-between px-4 pt-4'>
+                <div className='flex space-x-4'>
+                    <HeartIcon className='btn' />
+                    <ChatIcon className='btn' />
+                    <PaperAirplaneIcon className='btn' />
+                </div>
+
+                <BookmarkIcon className='btn' />
+            </div>
             {/*Captions*/}
+            <p className='p-5 truncate'>
+                <span className='font-bold mr-1'>{username}</span> {caption}</p>
             {/*Comments*/}
             {/*Input Box*/}
         </div>
